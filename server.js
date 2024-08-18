62c340693a615cc17bea3f9b38b0d11e965bbce4
Rapiwash <sistemarapiwash@gmail.com>
2024-08-17 22:40:34 -0500

    correcion cord

--- server.js
+++ server.js
@@ -46,11 +46,16 @@ const server = http.createServer(app);
 
 export const io = new SocketServer(server, {
   cors: {
-    origin: "*",
+    origin: "https://lavanderia-mrjeffdalias.onrender.com",
   },
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://lavanderia-mrjeffdalias.onrender.com",
+  })
+);
+
 app.use(express.json());
 
 io.on("connection", (socket) => {
